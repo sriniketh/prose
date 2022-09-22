cb8d919b60f2f9ef5b1d9bc5639b9b10f551d379
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2022-09-22 09:13:19 -0700

    use template for saved on string

--- feature-viewhighlights/src/main/java/com/sriniketh/feature_viewhighlights/HighlightViewHolder.kt
+++ feature-viewhighlights/src/main/java/com/sriniketh/feature_viewhighlights/HighlightViewHolder.kt
@@ -1,6 +1,7 @@
 package com.sriniketh.feature_viewhighlights
 
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.phrase.Phrase
 import com.sriniketh.feature_viewhighlights.databinding.HighlightItemCardBinding
 
 class HighlightViewHolder(
@@ -8,7 +9,8 @@ class HighlightViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(uiState: HighlightUIState) {
+        val context = binding.root.context
         binding.highlightText.text = uiState.text
-        binding.highlightSubtext.text = uiState.savedOn
+        binding.highlightSubtext.text = Phrase.from(context.getString(R.string.saved_on_template)).put("datetime", uiState.savedOn).format()
     }
 }
