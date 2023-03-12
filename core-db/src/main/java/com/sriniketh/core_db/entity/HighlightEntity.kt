d998590c04fd6c243f47892593ef4eff605a51c6
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2023-03-12 11:11:05 -0700

    update dependencies

--- core-db/src/main/java/com/sriniketh/core_db/entity/HighlightEntity.kt
+++ core-db/src/main/java/com/sriniketh/core_db/entity/HighlightEntity.kt
@@ -2,7 +2,7 @@ package com.sriniketh.core_db.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.ForeignKey.CASCADE
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
