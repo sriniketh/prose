04f605d02d1c25feef05e564ebb7851e671d51fb
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2024-04-27 12:37:17 -0700

    [feat] bookinfo screen

--- feature-searchbooks/src/main/java/com/sriniketh/feature_searchbooks/BookInfoFragmentViewModel.kt
+++ feature-searchbooks/src/main/java/com/sriniketh/feature_searchbooks/BookInfoFragmentViewModel.kt
@@ -9,6 +9,7 @@ import com.sriniketh.core_models.book.Book
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,46 +20,66 @@ class BookInfoFragmentViewModel @Inject constructor(
 ) : ViewModel() {
 
     private var _uiState: MutableStateFlow<BookInfoUiState> =
-        MutableStateFlow(BookInfoUiState.Initial)
+        MutableStateFlow(BookInfoUiState())
     internal val uiState: StateFlow<BookInfoUiState> = _uiState
 
     fun getBookDetail(volumeId: String) {
         viewModelScope.launch {
-            _uiState.emit(BookInfoUiState.Loading)
+            _uiState.update { state ->
+                state.copy(isLoading = true)
+            }
             val result = fetchBookInfoUseCase(volumeId)
             if (result.isSuccess) {
-                _uiState.emit(
-                    BookInfoUiState.BookInfoLoadSuccess(result.getOrThrow(), R.string.add_to_shelf_button_text) { book ->
-                        addBookToShelf(book)
-                    })
+                _uiState.update { state ->
+                    val book = result.getOrThrow()
+                    state.copy(
+                        isLoading = false,
+                        book = book,
+                        canAddToShelf = true, //TODO: determine if book was already added
+                        addBookToShelf = { addBookToShelf(book) }
+                    )
+                }
             } else if (result.isFailure) {
-                _uiState.emit(BookInfoUiState.Failure(R.string.book_info_load_error_message))
+                _uiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        snackBarText = R.string.book_info_load_error_message
+                    )
+                }
             }
         }
     }
 
     private fun addBookToShelf(book: Book) {
         viewModelScope.launch {
-            _uiState.emit(BookInfoUiState.Loading)
+            _uiState.update { state ->
+                state.copy(isLoading = true)
+            }
             val result = addBookToShelfUseCase(book)
             if (result.isSuccess) {
-                _uiState.emit(BookInfoUiState.AddToBookshelfSuccess(R.string.added_to_shelf_button_text))
+                _uiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        snackBarText = R.string.add_to_bookshelf_success_message,
+                        canAddToShelf = false
+                    )
+                }
             } else if (result.isFailure) {
-                _uiState.emit(BookInfoUiState.Failure(R.string.add_to_bookshelf_error_message))
+                _uiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        snackBarText = R.string.add_to_bookshelf_error_message
+                    )
+                }
             }
         }
     }
 }
 
-internal sealed interface BookInfoUiState {
-    object Initial : BookInfoUiState
-    object Loading : BookInfoUiState
-    data class BookInfoLoadSuccess(
-        val book: Book,
-        @StringRes val addBookToShelfButtonText: Int,
-        val addBookToShelf: (Book) -> Unit
-    ) : BookInfoUiState
-
-    data class AddToBookshelfSuccess(@StringRes val addBookToShelfButtonText: Int) : BookInfoUiState
-    data class Failure(@StringRes val errorMessage: Int) : BookInfoUiState
-}
+data class BookInfoUiState(
+    val isLoading: Boolean = false,
+    @StringRes val snackBarText: Int? = null,
+    val book: Book? = null,
+    val canAddToShelf: Boolean = false,
+    val addBookToShelf: () -> Unit = {}
+)
---
+++ feature-searchbooks/src/main/java/com/sriniketh/feature_searchbooks/BookInfoScreen.kt
@@ -0,0 +1,300 @@
+package com.sriniketh.feature_searchbooks
+
+import android.text.Html
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.sriniketh.core_design.ui.components.AppSurface
+import com.sriniketh.core_design.ui.components.NavigationBack
+import com.sriniketh.core_design.ui.components.gradientPlaceholder
+import com.sriniketh.core_design.ui.theme.AppTheme
+import com.sriniketh.core_models.book.Book
+import com.sriniketh.core_models.book.BookInfo
+import com.sriniketh.core_platform.buildHttpsUri
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun BookInfoScreen(
+    uiState: BookInfoUiState,
+    modifier: Modifier = Modifier,
+    goBack: () -> Unit
+) {
+    AppSurface {
+        val snackbarHostState = remember { SnackbarHostState() }
+        Scaffold(
+            modifier = modifier.fillMaxSize(),
+            topBar = {
+                LargeTopAppBar(
+                    title = { BookInfoScreenTitle(uiState) },
+                    navigationIcon = { NavigationBack(goBack) }
+                )
+            },
+            floatingActionButton = {
+                if (uiState.canAddToShelf) {
+                    BookInfoScreenFloatingActionButton(uiState.addBookToShelf)
+                }
+            },
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+        ) { contentPadding ->
+            if (uiState.isLoading) {
+                LinearProgressIndicator(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(contentPadding)
+                )
+            }
+
+            uiState.snackBarText?.let { resId ->
+                val snackbarMessage = stringResource(id = resId)
+                LaunchedEffect(key1 = null) {
+                    launch {
+                        snackbarHostState.showSnackbar(snackbarMessage)
+                    }
+                }
+            }
+
+            BookInfoLayout(uiState, modifier, contentPadding)
+        }
+    }
+}
+
+@Composable
+private fun BookInfoLayout(
+    uiState: BookInfoUiState,
+    modifier: Modifier,
+    contentPadding: PaddingValues
+) {
+    uiState.book?.let { book ->
+        val bookInfo = book.info
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(contentPadding)
+        ) {
+            Row(
+                modifier = modifier
+                    .padding(12.dp)
+                    .align(Alignment.Start)
+            ) {
+                val uri = bookInfo.thumbnailLink?.buildHttpsUri()
+                AsyncImage(
+                    modifier = modifier
+                        .padding(6.dp)
+                        .height(160.dp)
+                        .width(120.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    model = uri,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                    placeholder = gradientPlaceholder(),
+                    error = gradientPlaceholder()
+                )
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 6.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column {
+                        Text(
+                            modifier = modifier.padding(6.dp),
+                            text = bookInfo.authors.joinToString(", "),
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        bookInfo.publisher?.let { publisher ->
+                            Text(
+                                modifier = modifier.padding(6.dp),
+                                text = publisher,
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                        }
+                    }
+                }
+            }
+            bookInfo.description?.let { description ->
+                Card(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
+                ) {
+                    Text(
+                        modifier = modifier.padding(12.dp),
+                        text = Html.fromHtml(description, Html.FROM_HTML_MODE_LEGACY)
+                            .toString(),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+            Card(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
+            ) {
+                Column(modifier = modifier.padding(12.dp)) {
+                    val averageRating = bookInfo.averageRating
+                    val ratingsCount = bookInfo.ratingsCount
+                    if (averageRating != null && ratingsCount != null) {
+                        Text(
+                            modifier = modifier.padding(6.dp),
+                            text = stringResource(
+                                id = R.string.book_info_ratings_template,
+                                averageRating,
+                                ratingsCount
+                            ),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                    bookInfo.pageCount?.let {
+                        Text(
+                            modifier = modifier.padding(6.dp),
+                            text = stringResource(
+                                id = R.string.book_info_pagecount_template,
+                                it
+                            ),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                    bookInfo.publisher?.let {
+                        Text(
+                            modifier = modifier.padding(6.dp),
+                            text = stringResource(
+                                id = R.string.book_info_publisher_template,
+                                it
+                            ),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                    bookInfo.publishedDate?.let {
+                        Text(
+                            modifier = modifier.padding(6.dp),
+                            text = stringResource(
+                                id = R.string.book_info_publish_date_template,
+                                it
+                            ),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun BookInfoScreenFloatingActionButton(buttonOnClick: () -> Unit) {
+    ExtendedFloatingActionButton(
+        onClick = buttonOnClick,
+        icon = {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = stringResource(id = R.string.add_to_shelf_button_text)
+            )
+        },
+        containerColor = MaterialTheme.colorScheme.secondary,
+        contentColor = MaterialTheme.colorScheme.onSecondary,
+        text = { Text(text = stringResource(id = R.string.add_to_shelf_button_text)) }
+    )
+}
+
+@Composable
+private fun BookInfoScreenTitle(uiState: BookInfoUiState) {
+    Text(
+        text = if (uiState.book != null) {
+            uiState.book.info.title
+        } else {
+            stringResource(id = R.string.nav_label_book_info)
+        },
+        style = MaterialTheme.typography.headlineMedium,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+@PreviewLightDark
+@Composable
+internal fun BookInfoScreenPreview() {
+    AppTheme {
+        BookInfoScreen(
+            uiState = BookInfoUiState(
+                book = Book(
+                    id = "someId",
+                    info = BookInfo(
+                        title = "some really really really long title",
+                        subtitle = "some subtitle",
+                        authors = listOf("author 1, author 2"),
+                        thumbnailLink = "https://picsum.photos/200/300",
+                        publisher = "some publisher",
+                        publishedDate = "23rd January 2021",
+                        description = "some description that's repeated again and again so that we have this long piece of text. some description that's repeated again and again so that we have this long piece of text.",
+                        pageCount = 215,
+                        averageRating = 4.3,
+                        ratingsCount = 1227
+                    )
+                ),
+                addBookToShelf = {}
+            ),
+            goBack = {}
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+internal fun BookInfoScreenLoadingPreview() {
+    AppTheme {
+        BookInfoScreen(
+            uiState = BookInfoUiState(isLoading = true),
+            goBack = {}
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+internal fun BookInfoScreenSnackbarPreview() {
+    AppTheme {
+        BookInfoScreen(
+            uiState = BookInfoUiState(snackBarText = R.string.book_info_load_error_message),
+            goBack = {}
+        )
+    }
+}
