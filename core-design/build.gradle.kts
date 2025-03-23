d4dc363b6419c1b84e244616c42e45610faa80bb
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2025-03-23 00:21:30 -0700

    [kts-migration] migrate core-design module

---
+++ core-design/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	alias(libs.plugins.android.library)
+	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.kotlin.compose)
+}
+
+kotlin {
+	jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
+}
+
+android {
+	compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+	defaultConfig {
+		minSdk = libs.versions.minSdkVersion.get().toInt()
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
+
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	buildFeatures {
+		buildConfig = true
+		compose = true
+	}
+
+	namespace = "com.sriniketh.core_design"
+}
+
+dependencies {
+	implementation(libs.material)
+
+	val composeBom = platform(libs.compose.bom)
+	implementation(composeBom)
+	implementation(libs.bundles.compose)
+	debugImplementation(libs.compose.ui.tooling)
+	implementation(libs.google.fonts)
+
+	androidTestImplementation(composeBom)
+	androidTestImplementation(libs.compose.junit)
+	debugImplementation(libs.compose.test.manifest)
+
+	testImplementation(libs.junit)
+}
