ca2f49bb247f896f9c425c9b6edeea347745a9b8
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2025-03-23 00:23:42 -0700

    [kts-migration] migrate core-db module

---
+++ core-db/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+	alias(libs.plugins.android.library)
+	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.hilt)
+	alias(libs.plugins.ksp)
+}
+
+kotlin {
+	jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
+}
+
+android {
+	compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+	defaultConfig {
+		minSdk = libs.versions.minSdkVersion.get().toInt()
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
+
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	buildFeatures {
+		buildConfig = true
+	}
+	namespace = "com.sriniketh.core_db"
+}
+
+dependencies {
+	implementation(libs.android.core.ktx)
+
+	implementation(libs.room.runtime)
+	implementation(libs.room.ktx)
+	ksp(libs.room.compiler)
+
+	implementation(libs.hilt.android)
+	ksp(libs.hilt.compiler)
+
+	testImplementation(libs.junit)
+}
