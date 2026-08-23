717d68c592ca58940a5ded811456b71801e8926a
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2026-08-23 13:50:39 -0700

    [feat] use convention plugin for core-models

--- core-models/build.gradle.kts
+++ core-models/build.gradle.kts
@@ -1,10 +1,3 @@
 plugins {
-	alias(libs.plugins.java.library)
-	alias(libs.plugins.kotlin.jvm)
-}
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(libs.versions.jvmToolchainVersion.get().toInt())
-	}
+    id("prose.jvm.library")
 }
