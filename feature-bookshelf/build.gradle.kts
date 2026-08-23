636537d7c1b7963bc1052b60f8ebfbe574f6214e
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2026-08-23 14:05:02 -0700

    [feat] use convention plugin for feature-bookshelf

--- feature-bookshelf/build.gradle.kts
+++ feature-bookshelf/build.gradle.kts
@@ -1,71 +1,12 @@
 plugins {
-	alias(libs.plugins.android.library)
-	alias(libs.plugins.kotlin.compose)
-	alias(libs.plugins.hilt)
-	alias(libs.plugins.ksp)
-}
-
-kotlin {
-	jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
+    id("prose.android.feature")
 }
 
 android {
-	compileSdk = libs.versions.compileSdkVersion.get().toInt()
-
-	defaultConfig {
-		minSdk = libs.versions.minSdkVersion.get().toInt()
-
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		consumerProguardFiles("consumer-rules.pro")
-	}
-
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
-		}
-	}
-
-	buildFeatures {
-		viewBinding = true
-		buildConfig = true
-		compose = true
-	}
-
-	namespace = "com.sriniketh.feature_bookshelf"
+    namespace = "com.sriniketh.feature_bookshelf"
 }
 
 dependencies {
-
-	implementation(project(":core-design"))
-	implementation(project(":core-data"))
-	implementation(project(":core-models"))
-	implementation(project(":core-platform"))
-
-	implementation(libs.kotlinx.collections.immutable)
-	implementation(libs.coil)
-
-	val composeBom = platform(libs.compose.bom)
-	implementation(composeBom)
-	implementation(libs.bundles.compose)
-	debugImplementation(libs.compose.ui.tooling)
-
-	implementation(libs.lifecycle.runtime.compose)
-	implementation(libs.lifecycle.viewmodel.compose)
-	implementation(libs.hilt.lifecycle.viewmodel.compose)
-
-	androidTestImplementation(composeBom)
-	androidTestImplementation(libs.compose.junit)
-	debugImplementation(libs.compose.test.manifest)
-
-	implementation(libs.hilt.android)
-	ksp(libs.hilt.compiler)
-
-	testImplementation(libs.junit)
-	testImplementation(libs.coroutines.test)
-	testImplementation(libs.cashapp.turbine)
-	androidTestImplementation(libs.android.junit)
+    implementation(project(":core-platform"))
+    implementation(libs.coil)
 }
