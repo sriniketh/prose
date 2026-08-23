48d16221c781760e967886602fe93f70851473a5
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2026-08-23 14:03:33 -0700

    [feat] use convention plugin for core-design

--- core-design/build.gradle.kts
+++ core-design/build.gradle.kts
@@ -1,46 +1,12 @@
 plugins {
-	alias(libs.plugins.android.library)
-	alias(libs.plugins.kotlin.compose)
-}
-
-kotlin {
-	jvmToolchain(libs.versions.jvmToolchainVersion.get().toInt())
+    id("prose.android.library")
+    id("prose.android.compose")
 }
 
 android {
-	compileSdk = libs.versions.compileSdkVersion.get().toInt()
-
-	defaultConfig {
-		minSdk = libs.versions.minSdkVersion.get().toInt()
-
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		consumerProguardFiles("consumer-rules.pro")
-	}
-
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
-		}
-	}
-	buildFeatures {
-		buildConfig = true
-		compose = true
-	}
-
-	namespace = "com.sriniketh.core_design"
+    namespace = "com.sriniketh.core_design"
 }
 
 dependencies {
-	val composeBom = platform(libs.compose.bom)
-	implementation(composeBom)
-	implementation(libs.bundles.compose)
-	debugImplementation(libs.compose.ui.tooling)
-	implementation(libs.google.fonts)
-
-	androidTestImplementation(composeBom)
-	debugImplementation(libs.compose.test.manifest)
+    implementation(libs.google.fonts)
 }
